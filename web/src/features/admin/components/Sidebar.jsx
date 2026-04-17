d30c9822d266015ecd4cc396d2f017084b3036ba
zakariae-elyaakoubi <zakariaeelyaakoubi437@gmail.com>
2026-04-17 18:53:37 +0100

    one modification

--- web/src/features/admin/components/Sidebar.jsx
+++ web/src/features/admin/components/Sidebar.jsx
@@ -42,10 +42,7 @@ const Sidebar = () => {
       </button>
 
       {/* SYSTEM STATUS */}
-      <div className="flex items-center gap-2 text-xs text-white/50">
-        <span className="w-2 h-2 rounded-full bg-green-400 inline-block" />
-        All systems normal
-      </div>
+      
 
     </aside>
   );
