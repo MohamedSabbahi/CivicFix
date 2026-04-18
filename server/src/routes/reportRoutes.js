c5e5d4f606d079ee6816cf4a7e88088ce831813d
MohamedSabbahi <mohamed.sabbahi21@gmail.com>
2026-04-18 18:30:42 +0100

    feat: update report routes to use civicIssue model and adjust related methods

--- server/src/routes/reportRoutes.js
+++ server/src/routes/reportRoutes.js
@@ -1,37 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const { protect ,admin } = require('../middleware/authMiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware');
 const { validateReport } = require('../middleware/reportValidator'); 
-const { createReport,
-        updateStatusByMagicLink,
-        getAllReports,
-        getMyReports,
-        getNearbyReports,
-        getReportById,
-        updateReport ,
-        deleteReport,
-        getAllCategories,
-        assignDepartment,
-        showAssignDepartmentForm,
-        getReportDepartments }
-        = require('../controllers/reportController');
+const { 
+    createCivicIssue,        
+    updateStatusByMagicLink,
+    getAllCivicIssues,       
+    getMyCivicIssues,         
+    getNearbyCivicIssues,     
+    getCivicIssueById,        
+    updateCivicIssue,         
+    deleteCivicIssue,         
+    getAllCategories,
+    assignDepartment,
+    showAssignDepartmentForm,
+    getCivicIssueInterventions 
+} = require('../controllers/reportController');
 
-const { getReportComments,
-        createComment ,
-        deleteComment } 
-        = require('../controllers/commentController');
+const { 
+    getReportComments,
+    createComment,
+    deleteComment 
+} = require('../controllers/commentController');
 
-router.post('/', protect, upload.single('image'), validateReport, createReport);
+
+router.post('/', protect, upload.single('image'), validateReport, createCivicIssue);
 router.get('/categories', getAllCategories);
 router.get('/status-update', updateStatusByMagicLink);
-router.get('/', getAllReports);
-router.get('/nearby', getNearbyReports);
-router.get('/my-reports', protect, getMyReports);
-router.get('/:id' ,getReportById);
-router.get('/:id/departments', protect, getReportDepartments);
-router.put('/:id', protect, updateReport);
-router.delete('/:id', protect, admin, deleteReport);
+router.get('/', getAllCivicIssues);
+router.get('/nearby', getNearbyCivicIssues);
+router.get('/my-reports', protect, getMyCivicIssues);
+router.get('/:id', getCivicIssueById);
+router.get('/:id/departments', protect, getCivicIssueInterventions);
+router.put('/:id', protect, updateCivicIssue);
+router.delete('/:id', protect, admin, deleteCivicIssue);
 
 // Comment routes
 router.get('/:id/comments', getReportComments);
